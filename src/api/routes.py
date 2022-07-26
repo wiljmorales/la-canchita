c3b1535eee883bf40c77f2ae083fd75c6d180bed
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-07-26 23:54:27 +0000

    Se añadio login

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
+
 
 api = Blueprint('api', __name__)
 
@@ -28,4 +33,31 @@ def handle_users():
         "revise el payload de su solicitud"
         ), 400
     new_user = User(email, password)
-    return (new_user.serialize()), 201
+    return jsonify(new_user.serialize()), 201
+
+@api.route('/token', methods=['POST'])
+def create_token(): 
+    body = request.json
+    email = body.get('email', None)
+    password = body.get('password', None)
+    
+    if email is None or password is None:
+        return jsonify("revise el payload de su solicitud..."), 400
+    user = User.query.filter_by(email=email).one_or_none()
+    
+    if user is None or password != user.password:
+        return jsonify("password o usuario incorrecto"), 401
+    
+    access_token = create_access_token(identity=user.id)
+
+    return jsonify({
+        "token" : access_token
+    }), 201
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,65 +1,84 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			signUp: async (requestBody) => {
-				const response = await fetch(`${process.env.BACKEND_URL}/api/users`, {
-					method: "POST",
-					body: JSON.stringify(requestBody),
-					headers: {
-					"Content-Type":"application/json"
-				  	},
-				});
-				return response.status === 201;
-			},
+      signUp: async (requestBody) => {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/users`, {
+          method: "POST",
+          body: JSON.stringify(requestBody),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        return response.status === 201;
+      },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      login: async (requestBody) => {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/token`, {
+          method: "POST",
+          body: JSON.stringify(requestBody),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (!response.ok) throw Error("Hubo un problema con el login");
+        if (response.status === 401) {
+          throw "password o usuario incorrecto";
+        } else if (response.status === 400) {
+          throw "revise el payload de su solicitud...";
+        }
+        const data = await response.json();
+        localStorage.setItem("jwt-token", data.token);
+        return true;
+      },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+      getMessage: async () => {
+        try {
+          // fetching data from the backend
+          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+          const data = await resp.json();
+          setStore({ message: data.message });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
