0f548c4aeb8ec8f14c09a041802a42f4ae60fd94
Marius115 <cascaranomeister@gmail.com>
2022-07-29 07:09:07 +0000

    diseño de login y signup inicial.

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -9,52 +9,56 @@ export const Login = () => {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{ height: "100vh" }}
-      className="container flex-column d-flex align-items-center justify-content-center"
-    >
-      <div className="row">
-        <h1>Login de jugador</h1>
-      </div>
-      <div className="row">
-        <form>
-          <input
-            type="text"
-            name="email"
-            className="form-control my-1"
-            value={email}
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            name="password"
-            className="form-control my-1"
-            value={password}
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </form>
-      </div>
-      <div className="row">
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={async (e) => {
-            const success = await actions.login({
-              email: email,
-              password: password,
-            });
-            if (success) {
-              navigate("/private");
-              return;
-            }
-            alert("something happened while creating");
-          }}
+    <div className="portrait-login align-items-center justify-content-center">
+      
+        <div
+        style={{ height: "100vh" }}
+        className="container flex-column d-flex align-items-center justify-content-center"
         >
-          {"Login"}
-        </button>
+        <div className="row">
+          <h1>Login de jugador</h1>
+        </div>
+        <div className="row">
+          <form>
+            <input
+              type="text"
+              name="email"
+              className="form-control my-1"
+              value={email}
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              name="password"
+              className="form-control my-1"
+              value={password}
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </form>
+        </div>
+        <div className="row">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={async (e) => {
+              const success = await actions.login({
+                email: email,
+                password: password,
+              });
+              if (success) {
+                navigate("/private");
+                return;
+              }
+              alert("something happened while creating");
+            }}
+          >
+            {"Login"}
+          </button>
+        </div>
       </div>
     </div>
+    
   );
 };
--- src/front/js/pages/SignUp.js
+++ src/front/js/pages/SignUp.js
@@ -10,65 +10,68 @@ export const SignUp = (props) => {
     const navigate = useNavigate();
 
     return(
-        <div
-            style={{ height: "100vh" }}
-            className="container flex-column d-flex align-items-center justify-content-center">
-                <div className="row">
-                    <h1>Login de jugador</h1>
+        <div className="portrait-signup">
+            <div className="table align-items-center justify-content-center">
+                <div
+                    style={{ height: "100vh" }}
+                    className="container flex-column d-flex align-items-center justify-content-center">
+                    <div className="row">
+                        <h1 className="text-white">Signup de jugador</h1>
+                    </div>
+                    <div className="row">
+                        <form>
+                            <input
+                                type="text"
+                                name="name"
+                                className="form-control my-1"
+                                value={name}
+                                placeholder="name"
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <input
+                                type="text"
+                                name="email"
+                                className="form-control my-1"
+                                value={email}
+                                placeholder="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                className="form-control my-1"
+                                value={password}
+                                placeholder="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </form>
+                    </div>
+                    <div className="row">
+                        <button
+                            type="button"
+                            className="btn btn-success"
+                            onClick={async (e) => {
+                                const success = await actions.signUp({
+                                    name: name,
+                                    email: email,
+                                    password: password,
+                                });
+                                if (success) {
+                                navigate("/login");
+                                return;
+                                }
+                                alert("something happened while creating");
+                            }}
+                            >
+                            {"Sign up"}
+                        </button>
+                    </div>
                 </div>
-                <div className="row">
-                    <form>
-                    <input
-                            type="text"
-                            name="name"
-                            className="form-control my-1"
-                            value={name}
-                            placeholder="name"
-                            onChange={(e) => setName(e.target.value)}
-                        />
 
 
-                        <input
-                            type="text"
-                            name="email"
-                            className="form-control my-1"
-                            value={email}
-                            placeholder="email"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            className="form-control my-1"
-                            value={password}
-                            placeholder="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </form>
-                </div>
-            <div className="row">
-                <button
-                type="button"
-                className="btn btn-success"
-                onClick={async (e) => {
-                    const success = await actions.signUp({
-                        name: name,
-                        email: email,
-                        password: password,
-                    });
-                    if (success) {
-                    navigate("/login");
-                    return;
-                    }
-                    alert("something happened while creating");
-                }}
-                >
-                {"Sign up"}
-                </button>
             </div>
-
-
         </div>
+        
 
     )
 
