8879a209020ba020fd035788aae01e06e738631e
Marius115 <cascaranomeister@gmail.com>
2022-08-05 18:26:28 +0000

    :cat: arreglos menores en el Home.

--- src/front/js/component/Headlines.js
+++ src/front/js/component/Headlines.js
@@ -4,9 +4,56 @@ export const Headline = () => {
     return (
     <div className="container-fluid mt-5 mb-5 ">
      <div className="row">
-            <div className="col-6">
+            <div className="col-6 mb-3">
                 <div className="title d-flex"  >
-                    <h2 className="text-primary">Crea tus caimaneras</h2>
+                    <h2 className="text-primary">Crea Eventos!</h2>
+                    <br/>
+                </div>
+                <p className="text-primary">
+                    Crea eventos deportivos que los usuarios de la pagina podran ver y subscribirse
+                    para convertirse en encara messi que regatea por la izquierda, sigue messi messi 
+                    encara messi encara messi, y va messi por la banda que esta solo,Messi patea y 
+                    golazo de messi señores por dios que golazo.
+                </p>
+                </div>
+                <div className="col-6">
+                <img
+                src={`https://img.freepik.com/foto-gratis/cerrar-hombre-escribiendo-codigo-computadora-portatil_158595-5169.jpg?w=826&t=st=1659722620~exp=1659723220~hmac=8c49caafe9d89a091d42113806bfa46a4161b7023b4b4db837f66603a921f233`}
+                className="rounded-3 h-100 w-100"
+                alt="..."
+                />
+            </div>
+        </div>
+        <br/>
+
+        <div className="row">
+        <div className="col-5 mb-3">
+            <img
+            src={`https://www.bocopreps.com/wp-content/uploads/2021/04/BCC-L-LMonteraheader040121-01.jpg?w=620`}
+            className="rounded-3 h-100 w-100"
+            alt="..."
+        />
+            </div>
+            <div className="col-7">
+                <div className="title d-flex"  >
+                    <h2 className="text-primary">Encuentra Eventos!</h2>
+                    <br/>
+                </div>
+                <p className="text-primary">
+                    Subscribete a eventos que ya otros usuarios hayan creado previamente y busca los mas
+                    adecuados para ti para convertirse en encara messi que regatea por la izquierda, 
+                    sigue messi messi,encara messi encara messi, y va messi por la banda que esta solo,
+                    Messi patea y golazo de messi señores por dios que golazo.
+                </p>
+            </div>
+            
+        </div>
+        <br/>
+
+        <div className="row">
+            <div className="col-6 mb-3">
+                <div className="title d-flex"  >
+                    <h2 className="text-primary">Unete a la comunidad!</h2>
                     <br/>
                 </div>
                 <p className="text-primary">
@@ -25,29 +72,6 @@ export const Headline = () => {
             </div>
         </div>
         <br/>
-
-        <div className="row">
-        <div className="col-5">
-            <img
-            src={`https://www.bocopreps.com/wp-content/uploads/2021/04/BCC-L-LMonteraheader040121-01.jpg?w=620`}
-            className="rounded-3 h-100 w-100"
-            alt="..."
-        />
-            </div>
-            <div className="col-7">
-                <div className="title d-flex"  >
-                    <h2 className="text-primary">Inscribete a caimaneras</h2>
-                    <br/>
-                </div>
-                <p className="text-primary">
-                    Subscribete a eventos que ya otros usuarios hayan creado previamente y busca los mas
-                    adecuados para ti para convertirse en encara messi que regatea por la izquierda, 
-                    sigue messi messi,encara messi encara messi, y va messi por la banda que esta solo,
-                    Messi patea y golazo de messi señores por dios que golazo.
-                </p>
-            </div>
-            
-        </div>
 
 
     </div>
