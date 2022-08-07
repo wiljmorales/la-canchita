0f985729ed5fe8f6fb3c0a1ee7a0cf8c1f7ec740
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-08-07 01:17:23 +0000

    :star: Se creo el modal para mostrar las caimaneras del usuario actual

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -30,7 +30,49 @@ export const Navbar = () => {
               >
                 {"Cerrar sesión"}
               </button>
-              <button className="btn btn-warning">{"Mis Caimaneras"}</button>
+              <button
+                className="btn btn-warning"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                {"Mis Caimaneras"}
+              </button>
+              <div
+                className="modal fade"
+                id="exampleModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Mis Caimaneras
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">...</div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button type="button" className="btn btn-primary">
+                        Save changes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </>
           ) : (
             <>
